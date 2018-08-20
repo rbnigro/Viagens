dce143227df6a7ee40fef36ce423a63dd0ec1525
Ronney Nigro <ronneynigro@gmail,com>
2018-08-20 11:10:55 -0300

    Implementar Adapter

--- app/src/main/java/br/com/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -2,8 +2,10 @@ package br.com.aluraviagens.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import br.com.alura.viagem.R;
+import br.com.aluraviagens.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        listaDePacotes.setAdapter(new ListaPacotesAdapter());
     }
 }
